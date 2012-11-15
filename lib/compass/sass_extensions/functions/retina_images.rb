d292e691f860572325395ede53860d0913b7e391
Sebastian Zaha <sebastian.zaha@gmail.com>
2012-11-15 17:50:16 +0100

    Change filename pattern, fix file_exists check

--- lib/compass/sass_extensions/functions/retina_images.rb
+++ lib/compass/sass_extensions/functions/retina_images.rb
@@ -1,14 +1,13 @@
 module Compass::SassExtensions::Functions::RetinaImages
   def file_exists(filename)
     filename = filename.value
-    filename = File.join (Compass.configuration.images_path ? Compass.configuration.images_path : Compass.configuration.project_path), filename
+    filename = File.join (Compass.configuration.images_path ? Compass.configuration.images_path : Compass.configuration.project_path, filename)
     Sass::Script::Bool.new File.exists? filename
   end
 
   def retina_filename(filename)
     filename = filename.value
-    extension = filename.chomp! File.extname(filename)
-    Sass::Script::String.new "#{filename}@2x#{extension}"
+    Sass::Script::String.new filename.gsub('x1', 'x2')
   end
 end
 
