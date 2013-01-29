67ff3adb5dfe32a54ff753bb4a411ca8c1d404b1
Sebastian Zaha <sebastian.zaha@gmail.com>
2013-01-29 15:20:36 +0100

    Revert to initial filename convention.

--- lib/compass/sass_extensions/functions/retina_images.rb
+++ lib/compass/sass_extensions/functions/retina_images.rb
@@ -7,7 +7,8 @@ module Compass::SassExtensions::Functions::RetinaImages
 
   def retina_filename(filename)
     filename = filename.value
-    Sass::Script::String.new filename.gsub('x1', 'x2')
+    extension = File.extname(filename)
+    Sass::Script::String.new "#{filename.chomp(extension)}@2x#{extension}"
   end
 end
 
