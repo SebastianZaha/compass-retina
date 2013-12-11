75fc3637a678af7a78024cb372b465b7142d52d3
Sebastian Zaha <sebastian.zaha@gmail.com>
2013-12-11 12:20:27 +0100

    MM has different compass extension name.

--- lib/compass-retina.rb
+++ lib/compass-retina.rb
@@ -3,10 +3,9 @@ require "compass-retina/version"
 require "compass/sass_extensions/functions/retina_images"
 
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
-Compass::Frameworks.register('compass-retina', :path => extension_path)
+Compass::Frameworks.register('retina', :path => extension_path)
 
 module Compass
   module Retina
-    # Your code goes here...
   end
 end
